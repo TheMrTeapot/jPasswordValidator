8bf115e8c864f079eba858f7c235b3b2e07b324e
Rafa <rafael.robledo@coxautoinc.com>
2021-09-13 15:45:31 -0400

    Rafa-added comments to the class

--- src/com/ism510/PasswordForm.java
+++ src/com/ism510/PasswordForm.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionListener;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 
+/**
+ * PasswordForm is the GUI created that will hold the password field and the button
+ */
 public class PasswordForm {
     JButton validatePasswordButton;
     JPanel passwordView;
@@ -13,6 +16,10 @@ public class PasswordForm {
     final String PASSWORD_REGEX = "^(?=.*[0-9]){2,}(?=.*[a-z]){2,}(?=.*[A-Z]){2,}(?=\\S+$).{8,}$";
 
     public PasswordForm() {
+
+        /**
+         * This method will validate the password requirements via a regex check.
+         */
         validatePasswordButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
